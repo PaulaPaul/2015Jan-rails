8b1e07003fa0dc175d7daf51fe306a40522efdab
PaulaTTS <paula@techtalentsouth.com>
2015-02-18 14:19:22 +0000

    RailsU updates for faker and pagination of Student index

--- RailsU/app/models/course.rb
+++ RailsU/app/models/course.rb
@@ -1,5 +1,4 @@
 class Course < ActiveRecord::Base
   belongs_to  :department
   has_many   :enrollments
-  has_many  :instructors
 end
--- RailsU/app/models/instructor.rb
+++ RailsU/app/models/instructor.rb
@@ -1,3 +1,3 @@
 class Instructor < ActiveRecord::Base
-  has_many  :courses
+  has_many :departments
 end
